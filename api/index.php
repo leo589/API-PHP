5308974b80855f4a2847813f375991ac9d165e72
Ijika yuto <39280337+leo589@users.noreply.github.com>
2024-05-08 11:46:14 -0300

    Feat(PBI-01) archive api rest

---
+++ api/index.php
@@ -0,0 +1,30 @@
+<?php 
+$data = [];
+
+// Request
+if (isset($_GET['option'])) {
+    switch ($_GET['option']) {
+        case 'status':
+
+            $data['status'] = 'SUCCESS';
+            $data['data'] = 'API Running OK!';
+            break;
+
+        default:
+            $data['status'] = 'ERROR'
+        break;
+    }
+}else{
+    $data['status'] = 'ERROR'
+}
+
+//Emitir a resposta da API
+response($data);
+
+//Construção da Response
+function response($data_response)
+{
+    header("Content-Type:application/json");
+    echo json_encode($data_response);
+}
+?>
